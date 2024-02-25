a51cb33aa164e629be0b5d1e67262ccc2616255a
schaffer2013 <schaffer2013@gmail.com>
2024-02-24 19:44:57 -0800

    GCode test

--- _sandbox.py
+++ _sandbox.py
@@ -1,40 +1,11 @@
-from CardManage.Card import Card
-from CardManage.CardCompare import CardCompare
-from CardManage.Sorter import Sorter
+import gcodepy.src.gcodepy.gcode as gc
 
-s = Sorter (5)
+g = gc.Gcode(port = '')  
 
-print(s.sortingPiles)
+g.home()
+g.zero_extruder()
+g.set_bed_temp(90)
+g.draw(delta = (5, 4, 4), e = 0)
+g.draw(delta = (5, 4, 4), e = 0)
 
-cardNames = ["Azorius Charm","Boros Charm", "Gruul Charm",
-             "Eye of Ugin", "Mountain", "Shadowborne Apostle",
-             "Skullclamp", "Geist of Saint Traft", "Eerie Ultimatum",
-             "Ramos, Dragon Engine", "Island", "Plains", "Wastes",
-             "Piety Charm", "Colossal Dreadmaw", "The Kami War"]#,
-            # "Jungle Shrine", "Jetmir's Garden", "Savai Triome",
-            #  "Wastes", "City of Brass", "Ornithopter", "Squirrel", "Bone Saw",
-            #  "Cromat", "Clue", "Dryad Arbor", "Khalni Garden", "Command Tower",
-            #  "Arcane Signet", "Selesnya Guildgate"]
-
-index = 0
-cardNameLen = len(cardNames)
-
-while (index < cardNameLen):
-    s.registerTopCardOfPile(0, cardNames[index])
-    a = s.suggestMove()
-    s.transfer(a[0], a[1])
-    if a[0] == 0:
-        index += 1
-
-stillSorting = False
-for p in s.virtualTwinStacks:
-    if len(p) > 0:
-        stillSorting = True
-while (stillSorting == True and not s.stop):
-    for p in s.virtualTwinStacks:
-        if len(p) > 0:
-            stillSorting = True
-            break
-    a = s.suggestMove()
-    s.transfer(a[0], a[1])
-s.suggestMove()
+a = 1
